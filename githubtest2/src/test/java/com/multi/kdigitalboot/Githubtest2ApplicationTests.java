e1039b830d0fc87cfd2021c9bb4c49c1e0a490d1
Jihyeok11 <woah68@gmail.com>
2021-04-27 14:31:25 +0900

    add project

---
+++ githubtest2/src/test/java/com/multi/kdigitalboot/Githubtest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.kdigitalboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Githubtest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
